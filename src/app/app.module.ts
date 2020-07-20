6d8d8e6a071ba24c632f994ddd587b557012d041
unknown <mohit.tuli@oxfordcaps.com>
2020-07-20 20:07:50 +0530

    Updated Output propperty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule,
